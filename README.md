d070a7aef1c2b4cd99551929bcdab094ae99c034
xiang58 <danielxiang0508@gmail.com>
2020-02-11 22:17:05 -0600

    V1.2

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-##Best Fare Calculator V1.1
+## Best Fare Calculator V1.2 (2020-2-11)
 This project determines the best fare option for the user who visits large cities and plan to use its transit system.
+Currently, the city is set to NYC. The default transit system is MTA. The fare options were hard-coded, with the Pay-Per-Ride fare being $2.75; 7-day Unlimited Rides being $33.00; and 30-day Unlimited Rides being $127.00. [See MTA official website here for more information](https://new.mta.info/fares-and-tolls/subway-bus-and-staten-island-railway).
 
-####Release Notes
-- The fare options were hard-coded, with the Pay-Per-Ride fare being $2.75; 7-day Unlimited Rides being $33.00; and 30-day Unlimited Rides being $127.00. [See MTA official website here for more information](https://new.mta.info/fares-and-tolls/subway-bus-and-staten-island-railway).
-- Hashmap has been adopted to store the fare options, replacing array list.
-- User can now enter on the command line the number of days visiting and the number of expected rides. 
+#### Release Notes
+- Added reduced fare options for people over 65 years of age and/or have disabilities, with the Pay-Per-Ride fare being $1.35; 7-day Unlimited Rides being $16.50; and 30-day Unlimited Rides being $63.50
+- New feature: ensure the user's input is valid
+- New feature: user can select if s/he qualifies for reduecd fares
+- New feature: Repeat questions until user quits the program 
 
 *\* Project idea adopted from Codecademy*
--- TransitCalculator.java
+++ TransitCalculator.java
@@ -1,5 +1,5 @@
 /* @author Daniel Xiang
- * @version 1.1
+ * @version 1.2
  * @since 2020-02-11
  */
 
@@ -61,6 +61,8 @@ public class TransitCalculator {
 		return bestOption;
 	}
 	
+	
+	// Utils
 	public static void prt(String str, boolean newline) {
 		if (newline)
 			System.out.println(str);
@@ -68,26 +70,100 @@ public class TransitCalculator {
 			System.out.print(str);
 	}
 	
+	public static void greeting() {
+		prt("\nHi there, I'm Best Fare Calculator. I can help you ", false);
+		prt("determine which fare option is the cheapest!", true);
+		prt("(You can quit the program by typing 'quit' or 'q')", true);
+	}
+	
+	public static boolean checkInput(String input, String type) {
+		boolean valid = false;
+		if (type.equals("bool")) {
+			if (input.equals("yes") || input.equals("y") ||
+				input.equals("no")  || input.equals("n"))
+				valid = true;
+		}
+		else if (type.equals("num")) {
+			try {
+				int i = Integer.parseInt(input);
+				if (i > 0) valid = true;
+			} catch (Exception e) {
+				valid = false;
+			}
+		}
+		
+		return valid;
+	}
+	
 	
 	// Main
 	public static void main(String[] args) {
-		// Create hashmap to store fare options
-		fares.put("Pay-per-ride", 2.75);
-		fares.put("7-day Unlimited Rides", 33.00);
-		fares.put("30-day Unlimited Rides", 127.00);
-		
-		// Read user input
 		Scanner sc = new Scanner(System.in);
-		prt("How many days will you be using the transit system? ", false);
-		int days = sc.nextInt();
-		prt("The number of individual rides you expect to take during this period of time: ", false);
-		int rides = sc.nextInt();
+		String input = "";
 		
-		TransitCalculator tc = new TransitCalculator(rides, days);
-		HashMap<String, Double> prices = tc.getRidePrices();
-		String result = tc.getBestFare();
-		prt("You should get the " + result + " at $", false);
-		System.out.printf("%.2f per ride.", prices.get(result));
+		while (true) {
+			greeting();
+			
+			// To see if the user qualifies for reduced fares
+			prt("Do you qualify for the reduced fare option? ['yes' or 'y'/'no' or 'n'] ", true); 
+			prt("(Reduced fare is only for those who are at least 65 years old or have disability) ", false);
+			input = sc.nextLine();
+			if (input.equals("quit") || input.equals("q")) return;
+			
+			// Check for valid input
+			while (! checkInput(input, "bool")) {
+				prt("Invalid input. Please try again: ", false);
+				input = sc.nextLine();
+				if (input.equals("quit") || input.equals("q")) return;
+			}
+			
+			// Create hashmap to store fare options
+			if (input.equals("yes") || input.equals("y")) {
+				fares.put("Pay-per-ride", 1.35);
+				fares.put("7-day Unlimited Rides", 16.50);
+				fares.put("30-day Unlimited Rides", 63.50);
+			} 
+			else { // input == 'no' or 'n'
+				fares.put("Pay-per-ride", 2.75);
+				fares.put("7-day Unlimited Rides", 33.00);
+				fares.put("30-day Unlimited Rides", 127.00);
+			}
+			
+			// Read user input
+			prt("How many days will you be using the transit system? ", false);
+			input = sc.nextLine();
+			if (input.equals("quit") || input.equals("q")) return;			
+			while (! checkInput(input, "num")) {
+				prt("Invalid input. Please try again: ", false);
+				input = sc.nextLine();
+				if (input.equals("quit") || input.equals("q")) return;
+			}
+			int days = Integer.parseInt(input); 
+			
+			prt("Enter the number of individual rides you expect to take during this period of time: ", false);
+			input = sc.nextLine();
+			if (input.equals("quit") || input.equals("q")) return;			
+			while (! checkInput(input, "num")) {
+				prt("Invalid input. Please try again: ", false);
+				input = sc.nextLine();
+				if (input.equals("quit") || input.equals("q")) return;
+			}
+			int rides = Integer.parseInt(input);
+			
+			TransitCalculator tc = new TransitCalculator(rides, days);
+			HashMap<String, Double> prices = tc.getRidePrices();
+			String result = tc.getBestFare();
+			
+			prt("The prices are: ", true);
+			for (Map.Entry<String, Double> entry : prices.entrySet()) {
+				prt(entry.getKey() + " : ", false);
+				System.out.printf("$%.2f\n", entry.getValue());
+			}
+			
+			prt("Thus, you should get the option '" + result + "', which is equivelent to $", false);
+			System.out.printf("%.2f per ride.", prices.get(result));
+			prt("", true);
+		}
 	}
   
 }
